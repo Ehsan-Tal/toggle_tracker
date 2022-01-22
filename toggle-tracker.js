618e928278bf288c8b701e49bbf93cc7df784eab
Ehsan Mujahid <peme8meeng@gmail.com>
2022-01-22 16:45:16 +0400

    # [0.0.2] 'Barest Bones'
    ### Added
    -[Frontend] Exported JSON object to an external file - while retaining full use of it.
    -[Frontend] Added proper toggle functionality: the toggle affects the progress.
    -[Frontend] Added date functionality.
    -[Frontend] Separated CSS for small and large viewports.
    -[Frontend] Tinkered with the CSS.
    -[Frontend] Changed the section display from grid into flex.
    -[Frontend] Added download functionality.
    -[Frontend] Added querySelectors for other items.
    
    ### Fixed
    -[Frontend] An issue with spaces meant non-one-word ids returned errors.
    -[Frontend] Issue with local JSONs

--- toggle-tracker.js
+++ toggle-tracker.js
@@ -1,22 +1,183 @@
 
-// Create JSON - should go to another file
-
-// Get data from JSON.
-
+const header = document.querySelector('header');
 const section = document.querySelector('section');
-// we are still going to need to find a way to get the JSON object in a way that is not web-based.
+//const navbar = document.querySelector('#navbar');
+//const image = document.querySelector('#image');
+const footer = document.querySelector('footer');
+let JSONobj = {};
 
 //let json = JSON.parse('toggle-tracker.json');
-
-// Toggle-able buttons. The button part comes in populateSection()
-function toggler(butt){
-    console.log(butt);    
-    ( butt.getAttribute("class") === "on") ? butt.setAttribute("class", "off") : butt.setAttribute("class", "on");
-}
+// oh yeah, I complete forgot about loading the JSON in the first place.
 
 
-function populateSection(jsonObj){
-    const categories = jsonObj['categories'];
+/**
+ * this is a function to upload the JSON object to be used.
+ * 
+ * author: from a stackOverflow forum answer.
+ * 
+ */
+ async function uploadJSON(){
+
+    if (typeof window.FileReader !== 'function' ){
+        console.log("The file API isn't supported on this browser yet.")
+    
+        return
+    };
+    
+    let file, fileReading;
+    const input = document.getElementById('fileInput');
+    
+    if (!input){
+        console.log('There seems to not be an item of id fileInput.');
+    }
+    else if (!input.files){
+        console.log('There is no \'files\' property in the input tag.')
+    
+    }
+    else if (!input.files[0]){
+        console.log('There does not seem to be any file input.')
+    }
+    else {
+        file = input.files[0];
+        fileReading = new FileReader();
+        fileReading.onload = receivedText;
+        fileReading.readAsText(file);
+        console.log(file);
+    };
+    
+    function receivedText(e){
+        let lines = e.target.result;
+        const newArr = JSON.parse(lines);
+        console.log(newArr, 'newArr !');
+        JSONobj = newArr;
+    
+    }
+    await populateSection(JSONobj);
+    
+    //this should not be here as it creates repeats - also, it messes with it enough that it makes multiple items.
+    
+    };
+
+    
+/**
+ * this is a function to commit the changes of the JSON object to the JSON file.
+ * stingify stringifies the object into a string.
+ * 
+ * the rest sets a download and then executes that function.
+ * 
+ * I would like name to match up with the toggle-tracker.json through the browse file option.
+ * 
+ * author: Matěj Pokorný from a stackOverflow forum answer.
+ * 
+ * @param {Object} JSONobj 
+ * @param {String} name
+ */
+function downloadJSON(JSONobj, name){
+    !(name) ? name : name = 'toggle-tracker.json';
+    //!(JSONobj) ? JSONobj : JSONobj = temp_json;
+    console.log(JSONobj)
+    const JSONstr = JSON.stringify(JSONobj, null, '    ');
+    const element = document.createElement('a');
+
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(JSONstr));
+    element.setAttribute('download', name);
+    
+    element.style.display = 'none';
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+};
+
+
+/**
+ * this is a function given to each button built to change,
+ *  the progress value in the JSON object.
+ * Plus, change the CSS.
+ * 
+ * @param {object} button 
+ */
+function toggler(button) {
+
+    const category = button.dataset.category; // category
+    const component = button.dataset.component; //component
+    let decision = 0; // the critical value
+
+    if ( button.getAttribute("class") === "on") {
+        button.setAttribute("class", "off") 
+        decision = 0;
+    } else {
+        button.setAttribute("class", "on")
+        decision = 1;
+
+    };
+    
+    console.log("Setting this item\'s progress to", setJSONProgress(decision, category, component)); // we use all two c's to find out the right key
+    
+    // remember that console.log allows the command inside to alter data - but it uses the return value of that statement.
+    
+};
+
+
+/**
+ * this function checks to see if the name would have any space,
+ * it does so by checking if the split name is equal to the name,
+ * if not, that means there is space and so it re-assign a space-less value.
+ * 
+ * @param {string} name - this is the result of removing the space in between.
+ * @returns string
+ */
+const concatenator = function (name) {
+
+    ( name === name.split(' ', 1) ) ? name : name = name.split(' ').join('');
+    console.log(name);
+
+    return name;
+
+ }; 
+
+
+/**
+ * I might have this be consumed by the toggler.
+ * @param {int} newValue 
+ * @param {int} category 
+ * @param {int} component 
+ * @returns newValue
+ */
+function setJSONProgress (newValue, category, component){
+
+    JSONobj["categories"][category]["components"][component]["progress"] = newValue;
+    
+    return newValue
+};
+
+
+/**
+ * this gets the progress value,
+ *  although I am unsure as to what purpose it delivers currently.
+ * @param {int} category 
+ * @param {int} component 
+ * @returns value
+ */
+function getJSONProgress (category, component){
+    
+    const value = JSONobj["categories"][category]["components"][component]["progress"];
+
+    return value
+};
+
+
+/**
+ * populateSection takes in a JSON object,
+ * and uses it to create headings, buttons, and paragraphs,
+ * while also setting their distinguishing values and place in the hierarchy.
+ * 
+ * The return value should be null.
+ * 
+ * @param {object} JSONobj
+ */
+async function populateSection(JSONobj){
+    console.log(JSON['categories', 'populateSection'])
+    const categories = JSONobj['categories'];
 
     for ( let i = 0; i < categories.length; i++ ){
         const anArticle = document.createElement('article');
@@ -36,6 +197,9 @@ function populateSection(jsonObj){
             
             let anB = document.createElement('button');
             let anD = document.createElement('div');
+            console.log(components[j].name);
+
+            let processedName = concatenator(components[j].name); // captures the value part of the promise object.
 
             // The description can stay a P - behind the toggle.
             // The description should beneath the button in small viewports.
@@ -44,25 +208,43 @@ function populateSection(jsonObj){
             // the button's id could go through a function that concatenates them without space - or I could make it standard in the Json
             // Either way would require a function, so do the one that requires less JSON.
             anB.setAttribute("class", "off");
-            anB.setAttribute("id", components[j].name);
-            anB.setAttribute("onclick", `toggler(${components[j].name})`)
+            anB.setAttribute("id", processedName);
+            anB.setAttribute("data-category", String(i) );
+            anB.setAttribute("data-component", String(j) );
+            anB.setAttribute("onclick", `toggler(${processedName})`);
             anD.setAttribute("class", "description");
-            
+
             anB.textContent = components[j].name;
             anD.textContent = components[j].description;
 
             anArticle.appendChild(anB);
             //anArticle.appendChild(anD);
-            
-            
 
         }
-
         section.appendChild(anArticle);
     }
-}
+};
 
-// Set data to JSON.
+
+/**
+ * this is a function to populate the navigation bar,
+ * the one that houses the date and the settings button.
+ * 
+ */
+function populateNavbar(){
+
+};
+
+
+/**
+ * this is a function to populate the image portion,
+ * an item that may not even load depending on viewport size,
+ * but which houses a Napolean painting and a quote beneath.
+ * 
+ */
+function populateImage(){
+    const paintings = {};
+};
 
 
 // Date and Calendrical crap.
@@ -74,6 +256,14 @@ function populateSection(jsonObj){
 // Eventual Auto-toggles and hidden view.
 
 // Tests
+
+/**
+ * A series of tests.
+ * Remember to first think of the fail states !
+ */
+function testCode(){
+
+};
 
 
 let temp_json =    {
@@ -180,7 +370,7 @@ let temp_json =    {
                 "description" : "An Automatic Tendency.",
                 "components"  : [
                     { 
-                        name: "10 Push Ups a Day"        ,
+                        name: "Ten Push Ups a Day"        ,
                         progress: 0,
                         description: "Focus on Form; Establish a Routine. Ideal: 1progress: 0progress: 0 per day." 
                     },
@@ -233,4 +423,4 @@ let temp_json =    {
         ]
     }
 
-populateSection(temp_json);
+//populateSection();
